51220a3cd26c5576a297308d976f931c102d6019
j0nolan <jnolan@mayococo.ie>
2021-04-26 20:57:29 +0100

    "first commit"

---
+++ Import data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+## Import dataset
+dataset = pd.read_csv("Batchelor_degrees2017_18.csv")
+
+##print first 5 rows
+print(dataset.head())
+
+
+Categories = pd.read_csv("Batchelor_Degree_Categories.csv")
+
+
+
+
+
+
+
+
+
+
