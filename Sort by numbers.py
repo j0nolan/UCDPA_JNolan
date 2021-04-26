957de5a912064e4e5d7282e7da136c50debd3d15
j0nolan <jnolan@mayococo.ie>
2021-04-26 20:59:41 +0100

    "second commit"

---
+++ Sort by numbers.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv("Batchelor_degrees2017_18.csv")
+
+
+##find the missing values
+print(dataset.isnull().sum())
+
+##replace missing values
+cleaned_data = dataset.fillna(method='bfill', axis=0).fillna(0)
+print(cleaned_data.isnull().sum())
+print(dataset.shape)
+
+
+
+
+
+
+
+
+
