0c5a023197fd693ed1637c7bc7e5a2e69b84cf08
j0nolan <jnolan@mayococo.ie>
2021-04-26 21:01:11 +0100

    "third commit"

---
+++ Categories.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+## import dataset
+dataset = pd.read_csv("Batchelor_Degree_Categories.csv")
+##use indexing to find how many and which courses are in 10th position
+x = dataset.Subject[10]
+print(x)
+y = dataset.Courses[10]
+print(y)
+## use indexing to show first 10 records
+s = dataset
+print(s[:10])
